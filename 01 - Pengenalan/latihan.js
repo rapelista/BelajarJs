16687fff539f74fca386de72ff0f5e8f7c986f09
Farih Akmal Haqiqi <farih.282828@gmail.com>
2023-05-26 00:54:05 +0700

    latihan sampe tipe data

---
+++ 01 - Pengenalan/latihan.js
@@ -0,0 +1,24 @@
+// bikin variabel nama
+
+// bikin variabel umur
+
+// bikin variabel tempat tinggal
+
+// tampilin jadi kaya gini
+/*
+Halo nama gue Ikhsan
+Umur gue sekarang 17 tahun
+Dan gue tinggal di Cibinong.
+*/
+
+const nama = "Ikhsan";
+const umur = 17;
+const tempatTinggal = "Cibinong";
+
+console.log(`Halo nama gue ${nama}`);
+console.log(`Umur gue sekarang ${17} tahun`);
+console.log(`Dan gue tinggal di ${tempatTinggal}.`);
+
+console.log(`Halo nama gue ${nama}
+Umur gue sekarang ${umur} tahun
+Dan gue tinggal di ${tempatTinggal}.`);
